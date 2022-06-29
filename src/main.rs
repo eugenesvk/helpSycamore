d637a7ad04cb4d5b4a053f5aa21d5098c01160e1
eugenesvk <eugenesvk@users.noreply.github.com>
2022-06-29 15:02:33 +0700

    add Basics 3 Component

--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,51 @@ fn B2React<G:Html>(cx:Scope) -> View<G> { // fine-grained reactivity to keep the
   view! {cx, p{(view1)} p{(view2)} p{(view3)} p{(view4)}}
 }
 
+#[derive(Prop)] #[allow(dead_code)]
+struct MyProps {
+  name 	: String,
+  email	: String,
+}
+#[component] #[allow(dead_code)]
+fn B3Component<'a, G:Html>(cx:Scope, p:MyProps) -> View<G> { // see P0HelloWorld on the basics
+  // Properties. Allow accepting and rendering data from the parent.
+  // To allow your component to accept properties, take a second argument with a type that implements the Prop trait. For convenience, you can automatically derive the Prop trait with a derive-macro
+  let name  = p.name; 	//
+  let email = p.email;	//
+  let view1 = view! {cx, "Got properties in a MyProps struct with name `" (name) "` and email `" (email) "`"};
+
+  view! {cx, p{(view1)} }
+}
+
+#[derive(Prop)] #[allow(dead_code)]
+struct MyPropsReactData<'a> { value:&'a ReadSignal<i32> }
+#[component] #[allow(dead_code)]
+fn B3ComponentReactData<'a, G:Html>(cx:Scope<'a>, p:MyPropsReactData<'a>) -> View<G> {
+  // Reactive Properties
+  // For components to automatically react to prop changes, they should accept a signal. Most of the times, you’ll want a &ReadSignal unless you want mutable access to the data in which case you should use a &Signal. This way, updating the signal will automatically update whatever is listening to it, even if it is inside the child component
+
+  let view1 = view! {cx, div(class="my-component") {
+	  "@B3Component: Got properties in a MyPropsReactData struct with value:&ReadSignal `" (p.value.get()) }};
+  view! {cx, p{(view1)} }
+}
+#[component] #[allow(dead_code)]
+fn B3ComponentReactDataTest<G:Html>(cx:Scope) -> View<G> {
+  let state = create_signal(cx, 0);
+
+  let view1 = view! {cx, B3ComponentReactData { value:state }};
+  let view2 = view!{cx, button(on:click=|_| { state.set(2) }) {"@B3ComponentReactDataTest: Click me to set state that was passed to B3ComponentReactData =2"} };
+  let view3 = view!{cx, p };
+  let view4 = view!{cx, p };
+  view! {cx, p{(view1)} p{(view2)} p{(view3)} p{(view4)}}
+
+  // Component lifecycle is strongly tied to the reactive system, since, under the hood, components are simply functions that are run inside an untracked scope.
+  // This means that we can use the same helpers in the reactive system to attach callbacks to the component lifecycle.
+
+  // on_cleanup method schedules a callback to be called when the reactive scope is destroyed. This can also be used to schedule a callback when the component is destroyed.
+    // on_cleanup(cx, || { /* Perform cleanup. */    });
+}
+
+
 fn main() { // Trunk automatically uses fn main as your project's entrypoint, no need for any #[wasm_bindgen(start)] here
   sycamore::render( // ← provided by Sycamore to render your app to the DOM (browser window)
     // render accepts a closure (lambda function) which should return a view to be rendered
@@ -115,6 +160,8 @@ fn main() { // Trunk automatically uses fn main as your project's entrypoint, no
       } // Trunk just needs one thing to turn this into a website: a html source file to inject the view into
       // P0HelloWorld() // source the component declared above
       // B1View{}
-      B2React{}
+      // B2React{}
+      B3Component{name:"John Doe".to_string(), email:"john@doe.com".to_string()}
+      B3ComponentReactDataTest{}
   });
 }
